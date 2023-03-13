2ecb433fbdf375d07679b86e8968f524a886feaa
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-13 22:03:01 +0300

    06

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <div>one div</div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
