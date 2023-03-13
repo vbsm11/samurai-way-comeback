4f6210918c87b55b851b8a47bf15cb57e489a246
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-13 22:53:56 +0300

    10

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Technologies from './Technologies';
+import Footer from './Footer';
 
 function App() {
     return (
         <div>
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Header() {
     return (
         <div>
-            <a href={'#s'}>Home </a>
-            <a href={'#s'}>News Feed </a>
-            <a href={'#s'}>Messages </a>
+            <a href={'#s'}>Home</a> -
+            <a href={'#s'}>News Feed</a> -
+            <a href={'#s'}>Messages</a> -
         </div>
     );
 }
