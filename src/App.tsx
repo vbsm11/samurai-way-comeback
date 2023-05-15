502fda1b2bb1007ff0d9063fe254d122497de78a
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-05-15 21:45:39 +0300

    move browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
 import {Dialogs} from './components/Dialogs/Dialogs';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import {RootStateType} from './redux/state';
 
 type AppPropsType = {
@@ -13,16 +13,14 @@ type AppPropsType = {
 
 const App: React.FC<AppPropsType> = ({state}) => {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar navbarState={state.sidebar}/>
-                <div className="app-wrapper-content">
-                    <Route path='/profile' render={() => <Profile profileState={state.profilePage}/>}/>
-                    <Route path='/dialogs' render={() => <Dialogs dialogsState={state.dialogsPage}/>}/>
-                </div>
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar navbarState={state.sidebar}/>
+            <div className="app-wrapper-content">
+                <Route path="/profile" render={() => <Profile profileState={state.profilePage}/>}/>
+                <Route path="/dialogs" render={() => <Dialogs dialogsState={state.dialogsPage}/>}/>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
