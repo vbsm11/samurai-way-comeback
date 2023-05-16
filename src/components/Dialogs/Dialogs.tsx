62e2bcf4b24acc3e68373224ccc40249f2df88e4
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-05-16 06:48:09 +0300

    31

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -14,6 +14,12 @@ export const Dialogs: React.FC<DialogsPropsType> = ({dialogsState}) => {
 
     const messagesElements = dialogsState.messages.map(m => <Message message={m.message} isMy={m.isMy}/>)
 
+    const newMessageElement = React.createRef<HTMLTextAreaElement>()
+
+    const sentMessage = () => {
+        alert(newMessageElement.current?.value)
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -21,6 +27,10 @@ export const Dialogs: React.FC<DialogsPropsType> = ({dialogsState}) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div className={s.newMessage}>
+                <textarea ref={newMessageElement}/>
+                <button onClick={sentMessage}>Sent message</button>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,15 +12,23 @@ export const MyPosts: React.FC<MyPostsPropsType> = ({posts}) => {
 
     const postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
+    const newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    const addPost = () => {
+      alert(newPostElement.current?.value)
+    }
+
+    console.log(newPostElement)
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea/>
+                    <textarea ref={newPostElement}/>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
