8a782e7fc25edc15fc35cfd57549e938ad76aaa4
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-08-28 20:00:20 +0300

    deleting previous hardcode data, adding alt attribute to avatar

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,53 +7,6 @@ export const Users: React.FC<UsersPropsType> = (props) => {
     if (props.users.length === 0) {
         axios.default('https://social-network.samuraijs.com/api/1.0/users')
             .then(response =>  props.setUsers(response.data.items))
-
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         followed: true,
-        //         fullName: 'Georgiy',
-        //         photoUrl: 'https://pictures.sports.ru/NkyqqQSzzxQxqdz2ZOkG_Xw2tvjqW58GyH-E9nM_f00/fill/150/150/no/1/czM6Ly9zdGF0X3BpY3R1cmUvUExBWUVSL2F2YXRhci9nZW9yZ2lfZHpoaWtpeWEucG5n.png',
-        //         status: 'captain',
-        //         location: {
-        //             city: 'Balashikha',
-        //             country: 'Russia'
-        //         }
-        //     },
-        //     {
-        //         id: 2,
-        //         followed: true,
-        //         fullName: 'Quincy',
-        //         photoUrl: 'https://pictures.sports.ru/nqCrRC97O7ntFiQu0HuWC68cr2GPLkndIvflv8vwIXU/fill/150/150/no/1/czM6Ly9zdGF0X3BpY3R1cmUvUExBWUVSL2F2YXRhci9xdWluY3lfcHJvbWVzLnBuZw.png',
-        //         status: 'joker',
-        //         location: {
-        //             city: 'Amsterdam',
-        //             country: 'Netherlands'
-        //         }
-        //     },
-        //     {
-        //         id: 3,
-        //         followed: false,
-        //         fullName: 'Roman',
-        //         photoUrl: 'https://pictures.sports.ru/PJnwDEG50OgJKHtHle2aSqObd02fXZD-OuvtP_uzsfo/fill/150/150/no/1/czM6Ly9zdGF0X3BpY3R1cmUvUExBWUVSL2F2YXRhci9yb21hbl96b2JuaW4ucG5n.png',
-        //         status: 'machine',
-        //         location: {
-        //             city: 'Moscow',
-        //             country: 'Russia'
-        //         }
-        //     },
-        //     {
-        //         id: 4,
-        //         followed: false,
-        //         fullName: 'Alexandr',
-        //         photoUrl: 'https://pictures.sports.ru/7ZW3zTTA9MLECjntiQ9Uyth_aqt7qYImP0n3v6PJdWQ/fill/150/150/no/1/czM6Ly9zdGF0X3BpY3R1cmUvUExBWUVSL2F2YXRhci9hbGVrc2FuZHJfc29ib2xldi5wbmc.png',
-        //         status: 'striker',
-        //         location: {
-        //             city: 'Barnaul',
-        //             country: 'Russia'
-        //         }
-        //     }
-        // ])
     }
 
     return (
@@ -61,7 +14,7 @@ export const Users: React.FC<UsersPropsType> = (props) => {
             {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small? u.photos.small: ''} className={s.usersPhoto}/>
+                        <img src={u.photos.small? u.photos.small: ''} alt='avatar' className={s.usersPhoto}/>
                     </div>
                     <div>
                         {u.followed
