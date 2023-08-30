f8ed9c907a9d6d1b0f163ad323cca0797568d921
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-08-30 06:21:59 +0300

    moving class UsersApiComponent to UsersContainer file

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {UsersApiComponent} from './UsersApiComponent';
 import {connect} from 'react-redux';
 import {RootStateType} from '../../redux/redux-store';
 import {
@@ -11,7 +10,44 @@ import {
     UserType
 } from '../../redux/users-reducer';
 import {Dispatch} from 'redux';
+import * as axios from 'axios';
+import {Users} from './Users';
 
+
+export class UsersApiComponent extends React.Component<UsersPropsType> {
+
+    componentDidMount() {
+        axios.default(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+
+        axios.default(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => this.props.setUsers(response.data.items))
+    }
+
+
+    render() {
+
+        return (
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                users={this.props.users}
+
+                onPageChanged={this.onPageChanged}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />)
+
+    }
+}
 
 type MapStateToPropsType = {
     users: UserType[]
