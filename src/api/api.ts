948a6e1e943376fd68851c9bb7fd82e4d0d3987f
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-09-08 07:10:01 +0300

    create instance - axios.create(), baseUrl etc

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,26 +1,29 @@
 import * as axios from 'axios';
 
+const instance = axios.default.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'c6c4cf97-dba7-4d47-a853-30591c45efd6'
+    }
+})
+
 export const getUsers = (pageNumber: number, pageSize: number) => {
-  return axios.default(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`, {
-      withCredentials: true
-  }).then(response => response.data)
+    return instance.get(`users?page=${pageNumber}&count=${pageSize}`)
+        .then(response => response.data)
 }
 
 export const unfollowRequest = (id: number) => {
-    return axios.default.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
-        withCredentials: true
-    }).then(response => response.data)
+    return instance.delete(`follow/${id}`)
+        .then(response => response.data)
 }
 
 export const followRequest = (id: number) => {
-    return axios.default.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-        withCredentials: true
-    }).then(response => response.data)
+    return instance.post(`follow/${id}`)
+        .then(response => response.data)
 }
 
 export const auth = () => {
-    return axios.default('https://social-network.samuraijs.com/api/1.0/auth/me', {
-        withCredentials: true
-    })
+    return instance.get('auth/me')
         .then(response => response.data)
 }
