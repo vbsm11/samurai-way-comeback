e9877c37f9db57c34032241ad692dd4754c7003d
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-09-20 06:59:19 +0300

    adding applyMiddleware to createStore, importing thunkMiddleWare from 'redux-thunk'

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {ProfileActionType, profileReducer} from './profile-reducer';
 import {DialogsActionType, dialogsReducer} from './dialogs-reducer';
 import {SidebarActionType, sidebarReducer} from './sidebar-reducer';
 import {UsersActionType, usersReducer} from './users-reducer';
 import {AuthActionType, authReducer} from './auth-reducer';
+import thunkMiddleWare from 'redux-thunk'
 
 export type ActionType = ProfileActionType | DialogsActionType | SidebarActionType | UsersActionType | AuthActionType
 
@@ -17,7 +18,7 @@ const rootReducer = combineReducers({
 
 export type RootStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare))
 
 // @ts-ignore
 window.store = store
